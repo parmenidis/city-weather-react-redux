0d131928d1dcb94a568ffad71d1c6c90241b13e7
Oleksandr_Shustrov <oleksandr.shustrov@racingpost.com>
2017-02-27 15:02:43 +0200

    preparing to API request

--- src/containers/searchBar.js
+++ src/containers/searchBar.js
@@ -7,13 +7,34 @@ class SearchBar extends Component {
 
         this.state = {
             term: ''
+        };
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        //console.log(event.target.value);
+
+        if( event.target.value ) {
+            this.setState({
+                term: event.target.value
+            });
         }
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
     }
 
     render() {
         return (
-            <form className="input-group">
-                <input value={this.state.term}/>
+            <form className="input-group" onSubmit={this.onFormSubmit}>
+                <input
+                    value={this.state.term ? this.state.term : ''}
+                    placeholder="Get a five-day forecast in your favorite cities."
+                    className="form-control"
+                    onChange={this.onInputChange}
+                />
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">
                         Search
